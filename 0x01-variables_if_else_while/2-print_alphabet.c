a71b1f54593acfe6b85cab1cb80dced0cca64573
ekow77 <ekowbrightblessedsanchez@gmail.com>
2023-02-17 05:24:43 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stlib.h>
+/**
+ * main - main block
+ * Description: Get a random it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char C = 'a';
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
