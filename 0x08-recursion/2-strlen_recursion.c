22be34659250aa5d9a12cdb479a37c0844c18229
ekow77 <ekowbrightblessedsanchez@gmail.com>
2023-03-08 09:19:53 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,15 +5,15 @@
  *
  * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
 {
-int longit = 0;
-if (*s)
-{
-longit++;
-longit++;
-longit += _strlen_recursion(s + 1);
-}
-return (longit);
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
 }
