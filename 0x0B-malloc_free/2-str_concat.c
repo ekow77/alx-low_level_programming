5e6390a32bc8f6f01cc63e4d0660d63bf90fb322
ekow77 <ekowbrightblessedsanchez@gmail.com>
2023-03-13 09:00:49 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,38 +12,39 @@
 char *str_concat(char *s1, char *s2)
 
 {
-	char *conct;
-		int i, ci;
-	if (s1 == NULL)
 
-		s1 = "";
-	if (s2 == NULL)
+char *conct;
 
-		s2 = "";
-		i = ci = 0;
+int i, ci;
 
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
+if (s1 == NULL)
 
-	conct = malloc(sizeof(char) * (i + ci + 1));
-	if (conct == NULL)
-		return (NULL);
-	i = ci = 0;
+s1 = "";
 
-	while (s1[i] != '\0')
+if (s2 == NULL)
+s2 = "";
+i = ci = 0;
+while (s1[i] != '\0')
+i++;
+while (s2[ci] != '\0')
 
-	{
-		conct[i] = s1[i];
-		i++;
-	}
+ci++;
+conct = malloc(sizeof(char) * (i + ci + 1));
+if (conct == NULL)
+return (NULL);
+i = ci = 0;
+while (s1[i] != '\0')
 
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+{
+conct[i] = s1[i];
+i++;
+}
+while (s2[ci] != '\0')
+{
+conct[i] = s2[ci];
+i++, ci++;
+}
+conct[i] = '\0';
+return (conct);
+
 }
